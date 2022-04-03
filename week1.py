4d65695dc3a22c9cb42cec11c13053bcfc08bf3a
RaistlinMingZ <raistlinming@outlook.com>
2022-04-03 20:22:39 +0930

    w1

---
+++ week1.py
@@ -0,0 +1,21 @@
+#1
+#def distance(velocity, time):
+#    distance = velocity * time
+#    return distance
+
+#2
+#def kinetic_energy(mass, velocity):
+#    KE = 0.5 * mass * velocity**2
+#    return KE
+
+#3
+# num1 = int(input('Please enter a number: '))
+
+# def is_prime(num):
+#     result = True
+#     if num % 2 == 0:
+#         result = False
+#     return result
+
+# res = is_prime(num1)
+# print('The result is:', res)
